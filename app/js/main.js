91d46a1cb0be2049da725b01759890e270c31664
Alexeykormiltsev <cormiltsev@yandex.ru>
2022-03-27 19:21:14 +0500

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,11 @@ $('.top-slider__inner').slick({
  autoplay: true,
  autoplaySpeed: 2000
 });
+$(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
 
 });
